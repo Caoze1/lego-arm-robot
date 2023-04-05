3157e956cf6e5e5aec033cf38dcf129affb3975f
Caoze1 <58563246+Caoze1@users.noreply.github.com>
2023-04-05 14:00:50 +0200

    Start

--- master.py
+++ master.py
@@ -1,2 +1,4 @@
-"""woop woop test nr2"""
-
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, TouchSensor, ColorSensor
+from pybricks.parameters import Port, Stop, Direction
+from pybricks.tools import wait
